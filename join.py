a450f6148592a0799695584c38c6c881fd692c99
Muhammed Mahdi <64995259+muh3ammed@users.noreply.github.com>
2021-09-26 19:51:12 +0300

    join() method

---
+++ join.py
@@ -0,0 +1,16 @@
+users = ['ali', 'assaf', 'muhammed']
+
+# Method 1
+print(users[0]+','+users[1]+','+users[2])
+
+# Method 2
+for i in range(len(users)):
+
+  if i != len(users)-1:
+  
+    print(users[i]+',', end='')
+    
+  else:print(users[i])
+
+# Method 3
+print(','.join(users))
