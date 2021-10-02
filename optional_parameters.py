47bbd022f344c4ac82751cff6510cb7a368fc4ee
Muhammed Mahdi <64995259+muh3ammed@users.noreply.github.com>
2021-10-02 15:39:32 +0300

    Optional Parameters

---
+++ optional_parameters.py
@@ -0,0 +1,20 @@
+class Student(object):
+  
+  def __init__(self, name, age, country='null'):
+    
+    self.name = name
+    self.age = age
+    self.country = country
+    
+  def info(self, show_all=True):
+    
+    if show_all:
+      
+      print(f'{self.name}, {self.age}, {self.country}')
+      
+    else:print(self.name)
+      
+
+student_1 = Student('muhammed', 21)
+student_1.info()
+student_1.info(False)
